68e42f2c88b29e1807a49410d035522cf921f315
Igor Scherba <i.scherba32@gmail.com>
2022-07-04 10:48:43 +0300

    removed CORS

--- webpack.config.js
+++ webpack.config.js
@@ -13,11 +13,6 @@ module.exports = (webpackConfigEnv, argv) => {
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
     module: {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-      },
       rules: [
         {
           test: /\.(scss|css)$/i,
