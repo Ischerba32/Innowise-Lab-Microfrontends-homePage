a4e6d0601e4c3df75be8bf52f764dd00fa67f196
Igor Scherba <i.scherba32@gmail.com>
2022-07-05 11:59:58 +0300

    added loader component to authorization check

--- src/components/AuthRoute/index.tsx
+++ src/components/AuthRoute/index.tsx
@@ -6,29 +6,37 @@ import { auth } from '../../config/firebaseConfig';
 import IUser from '../../interfaces/user.interface';
 import { AuthContext } from '../../context/authContext';
 import AuthRouteProps from './props';
+import { Loader } from '../UI';
 
 const AuthRoute = ({ children }: AuthRouteProps) => {
 	const [user, setUser] = useState<IUser>({ uid: '', email: '' });
-
+	const [loading, setLoading] = useState(true);
 	const navigate = useNavigate();
 
 	useEffect(() => {
 		return onAuthStateChanged(auth, (user) => {
+			setLoading(true);
 			if (user) {
 				setUser({
 					uid: user.uid,
 					email: user?.email,
 				});
+				setTimeout(() => setLoading(false), 1000);
 			} else {
 				console.log('Unauthorized');
 				setUser({
 					uid: '',
 					email: '',
 				});
-				navigate('/signin');
+				setTimeout(() => {
+					navigate('/signin');
+					setLoading(false);
+				}, 1000);
 			}
 		});
 	}, [navigate]);
+
+	if (loading) return <Loader speed={2} />;
 
 	return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
 };
